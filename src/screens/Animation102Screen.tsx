f9d3b80ff8f9b0b27dbe49b8fc34abefbae72868
EcheCostabel <exequielcostabel@gmail.com>
2023-05-25 19:34:02 -0300

    animatedValueZY

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, View, Animated, PanResponder } from 'react-native';
 
 export const Animation102Screen = () => {
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: pan.x, // x,y are Animated.Value
+        dy: pan.y,
+      },
+    ], {useNativeDriver: false}),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+      ).start();
+    },
+  });
+
+
   return (
     <View style={styles.container}>
-        <View  style={styles.purpleBox}/>
+        <Animated.View  
+           {...panResponder.panHandlers}
+           style={[pan.getLayout(), styles.purpleBox]}
+        />
     </View>
   )
 };
@@ -17,7 +41,7 @@ const styles = StyleSheet.create({
       alignItems: 'center'
     },
     purpleBox: {
-        backgroundColor: 'red',
+        backgroundColor: '#75CED8',
         width: 150,
         height: 150
     }
