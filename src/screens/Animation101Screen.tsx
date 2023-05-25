262b78fc1794082418fc90253930a4651756fcfd
EcheCostabel <exequielcostabel@gmail.com>
2023-05-25 18:33:18 -0300

    fadeIn and fadeOut

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,19 +1,50 @@
 import React, { useRef } from 'react';
-import { StyleSheet, View, Animated } from 'react-native';
+import { StyleSheet, View, Animated, Button } from 'react-native';
 
 export const Animation101Screen = () => {
 
     const opacity = useRef(new Animated.Value(0.4)).current;
 
+    const fadeIn = ( ) => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 1,
+                duration: 300,
+                useNativeDriver: true
+            }
+        ).start();
+    };
+
+
+    const fadeOut = ( ) => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 0.4,
+                duration: 300,
+                useNativeDriver: true
+            }
+        ).start();
+    };
 
 
   return (
     <View style={styles.container}>
         <Animated.View  style={{
             ...styles.purpleBox,
+            marginBottom: 20,
             opacity: opacity
-            
             }}/>
+
+            <Button
+                title='FadeIn'
+                onPress={fadeIn}
+            />
+            <Button
+                title='FadeOut'
+                onPress={fadeOut}
+            />
     </View>
   )
 };
