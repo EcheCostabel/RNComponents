b2353e6afd307b9b62fae0be6c8816135b7f0102
EcheCostabel <exequielcostabel@gmail.com>
2023-05-25 19:17:21 -0300

    useAnimation

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,53 +1,13 @@
 import React, { useRef } from 'react';
 import { StyleSheet, View, Animated, Button, Easing } from 'react-native';
+import { useAnimation } from '../hooks/useAnimation';
 
 export const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0)).current;
-    const top = useRef(new Animated.Value(-100)).current;
-
-    const fadeIn = ( ) => {
-        Animated.timing(
-            opacity,
-            {
-                toValue: 1,
-                duration: 300,
-                useNativeDriver: true
-            }
-        ).start();
-
-        Animated.timing(
-            top,
-            {
-                toValue: 0,
-                duration: 800,
-                useNativeDriver: true,
-                easing: Easing.bounce
-            }
-        ).start();
-    };
-
-
-
-
-    const fadeOut = ( ) => {
-        Animated.timing(
-            opacity,
-            {
-                toValue: 0,
-                duration: 300,
-                useNativeDriver: true
-            }
-        ).start();
-
-
-        
-
-        
-    };
+    
+    const { fadeIn, fadeOut, opacity, position, startMovingPosition} = useAnimation();
 
     
-
   return (
     <View style={styles.container}>
         <Animated.View  style={{
@@ -55,14 +15,17 @@ export const Animation101Screen = () => {
             marginBottom: 20,
             opacity: opacity,
             transform: [{
-                translateY: top
+                translateY: position
             }]
             
             }}/>
 
             <Button
                 title='FadeIn'
-                onPress={fadeIn}
+                onPress={() => (
+                    fadeIn(),
+                    startMovingPosition( -100 )
+                )}
             />
             <Button
                 title='FadeOut'
