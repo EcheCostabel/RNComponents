a0b110d2237654f7c22071e97b2b7cdaae75f58b
EcheCostabel <exequielcostabel@gmail.com>
2023-05-25 19:00:02 -0300

    Easing Bounce

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,9 +1,10 @@
 import React, { useRef } from 'react';
-import { StyleSheet, View, Animated, Button } from 'react-native';
+import { StyleSheet, View, Animated, Button, Easing } from 'react-native';
 
 export const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0.4)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+    const top = useRef(new Animated.Value(-100)).current;
 
     const fadeIn = ( ) => {
         Animated.timing(
@@ -14,27 +15,49 @@ export const Animation101Screen = () => {
                 useNativeDriver: true
             }
         ).start();
+
+        Animated.timing(
+            top,
+            {
+                toValue: 0,
+                duration: 800,
+                useNativeDriver: true,
+                easing: Easing.bounce
+            }
+        ).start();
     };
+
+
 
 
     const fadeOut = ( ) => {
         Animated.timing(
             opacity,
             {
-                toValue: 0.4,
+                toValue: 0,
                 duration: 300,
                 useNativeDriver: true
             }
         ).start();
+
+
+        
+
+        
     };
 
+    
 
   return (
     <View style={styles.container}>
         <Animated.View  style={{
             ...styles.purpleBox,
             marginBottom: 20,
-            opacity: opacity
+            opacity: opacity,
+            transform: [{
+                translateY: top
+            }]
+            
             }}/>
 
             <Button
