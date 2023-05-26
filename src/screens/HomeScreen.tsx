e8f79be6978a0864d684b58bd0db628898ea09ae
EcheCostabel <exequielcostabel@gmail.com>
2023-05-26 12:23:21 -0300

    Switch

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { Text, View, FlatList } from 'react-native';
-import  Icon  from 'react-native-vector-icons/Ionicons';
 import { styles } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { MenuItem } from '../interfaces/appInterfaces';
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
+import { menuItems } from '../data/menuItems';
 
 
-
-const menuItems: MenuItem[] = [
-    {
-        name: 'Animation 101',
-        icon: 'cube-outline',
-        component: 'Animation101Screen'
-    },
-    {
-        name: 'Animation 102',
-        icon: 'albums-outline',
-        component: 'Animation102Screen'
-    },
-]
 
 
 export const HomeScreen = () => {
---
+++ src/screens/SwitchScreen.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Platform, Switch, Text, View } from 'react-native'
+
+export const SwitchScreen = () => {
+
+    const [ isEnabled, setIsEnabled ] = useState(false);
+    const toggleSwitch = () => setIsEnabled(!isEnabled);
+
+  return (
+    <View style={{marginTop: 100}}>
+         <Switch
+            trackColor={{false: '#D9D9DD', true: '#5856D6'}}
+            thumbColor={(Platform.OS === 'android') ? '#5856D6' : ''}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+      />
+    </View>
+  )
+};
