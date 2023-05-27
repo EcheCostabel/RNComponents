55c2c4c829e727e3568947343c82ec3975e44eef
EcheCostabel <exequielcostabel@gmail.com>
2023-05-27 12:14:28 -0300

    adding prompt

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Alert, Button, View } from 'react-native'
-import { HeaderTitle } from '../components/HeaderTitle'
-import { styles } from '../theme/appTheme'
+import React from 'react';
+import { Alert, Button, View } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
+import { styles } from '../theme/appTheme';
+import prompt from 'react-native-prompt-android';
 
 export const AlertScreen = () => {
 
@@ -18,7 +19,24 @@ export const AlertScreen = () => {
             {text: 'OK', onPress: () => console.log('OK Pressed')}
         ]
     )
-}
+};
+
+    const showPrompt = ( ) => {
+        // prompt(
+        //     'Enter password',
+        //     'Enter your password to claim your $1.5B in lottery winnings',
+        //     [
+        //      {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        //      {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
+        //     ],
+        //     {
+        //         type: 'secure-text',
+        //         cancelable: false,
+        //         defaultValue: 'test',
+        //         placeholder: 'placeholder'
+        //     }
+        // );
+    }
 
   return (
     <View style={styles.globalMargin}>
@@ -27,6 +45,11 @@ export const AlertScreen = () => {
         <Button 
             title='Show Alert'
             onPress={showAlert}
+        />
+        <View  style={{height: 10}}/>
+        <Button 
+            title='Show Prompts'
+            onPress={showPrompt}
         />
     </View>
   )
