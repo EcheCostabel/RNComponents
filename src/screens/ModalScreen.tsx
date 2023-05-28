cbdb65721226e917347ff58c85aa26e7e164e4d5
EcheCostabel <exequielcostabel@gmail.com>
2023-05-28 20:14:55 -0300

    Modal

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -26,15 +26,28 @@ export const ModalScreen = () => {
                 flex: 1,
                 // width:100,
                 // height: 100,
-                backgroundColor: 'rgba(0,0,0,0.3)'
+                backgroundColor: 'rgba(0,0,0,0.3)',
+                justifyContent: 'center',
+                alignItems: 'center'
             }}>
 
             {/* CONTENIDO DEL MODAL */}
             <View style={{
-                backgroundColor: 'white'
+                backgroundColor: 'white',
+                width: 200,
+                height: 200,
+                justifyContent: 'center',
+                alignItems: 'center',
+                shadowOffset: {
+                    width: 0,
+                    height: 10,
+                },
+                shadowOpacity: 0.25,
+                elevation: 10,
+                borderRadius: 10
             }}>
-                <HeaderTitle title='Modal' />
-                <Text>Cuerpo del modal</Text>
+                <Text style={{fontSize: 20, fontWeight: 'bold'}}>Modal</Text>
+                <Text style={{fontSize: 16, fontWeight: '300', marginBottom: 20}}>Cuerpo del modal</Text>
 
                 <Button 
                     title='cerrar modal'
